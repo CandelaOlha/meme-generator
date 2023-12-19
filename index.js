f44e64358d1c5d1fd9a3d4476f031b235cb4b752
“c.b.olha” <c.b.olha@viseven.com>
2023-12-19 15:52:16 -0300

    applied some image settings & refactoring

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const closeImageEditor = document.querySelector("#closeImageEditor");
-const closeTextEditor = document.querySelector("#closeTextEditor");
+const closeImageEditorButton = document.querySelector(
+  "#closeImageEditorButton"
+);
+const closeTextEditorButton = document.querySelector("#closeTextEditorButton");
+const editMemeImageButton = document.querySelector("#editMemeImageButton");
+const editMemeTextButton = document.querySelector("#editMemeTextButton");
 const asideImageEditor = document.querySelector("#asideImageEditor");
 const asideTextEditor = document.querySelector("#asideTextEditor");
+const imageForm = document.querySelector("#imageForm");
+const memeImage = document.querySelector(".meme-image");
+const urlInput = document.querySelector("#urlInput");
+const imageBackgroundColorInput = document.querySelector(
+  "#imageBackgroundColorInput"
+);
 
-closeImageEditor.onclick = () => {
+// Open and close aside forms
+
+const closeImageEditor = () => {
   asideImageEditor.classList.add("hidden");
   asideTextEditor.classList.remove("hidden");
 };
 
-closeTextEditor.onclick = () => {
+closeImageEditorButton.addEventListener("click", closeImageEditor);
+editMemeTextButton.addEventListener("click", closeImageEditor);
+
+const closeTextEditor = () => {
   asideTextEditor.classList.add("hidden");
   asideImageEditor.classList.remove("hidden");
+};
+
+closeTextEditorButton.addEventListener("click", closeTextEditor);
+editMemeImageButton.addEventListener("click", closeTextEditor);
+
+imageForm.onsubmit = (e) => {
+  e.preventDefault();
+};
+
+// Image settings
+
+urlInput.onchange = () => {
+  const backgroundImage = urlInput.value;
+  memeImage.style.backgroundImage = `url(${backgroundImage})`;
+};
+
+imageBackgroundColorInput.oninput = () => {
+  const backgroundColor = imageBackgroundColorInput.value;
+  memeImage.style.backgroundColor = backgroundColor;
 };
